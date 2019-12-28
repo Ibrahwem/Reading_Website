1c0b9f903ca7427811eff2b2fabe62b716129eef
Ibrahem Wattaweda <wattad111@gmail.com>
2019-12-28 13:23:24 +0200

    adding a dropdownlist tow the animals stories in order to cahmge the story coloe(letters).

--- Learningweb/CrocodileAndTheMonkey.aspx.cs
+++ Learningweb/CrocodileAndTheMonkey.aspx.cs
@@ -13,5 +13,149 @@ namespace Learningweb
         {
 
         }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+             if (DropDownList2.SelectedIndex.Equals(1))
+            {
+                Label1.ForeColor = System.Drawing.Color.Black;
+                Label2.ForeColor = System.Drawing.Color.Black;
+                Label3.ForeColor = System.Drawing.Color.Black;
+                Label4.ForeColor = System.Drawing.Color.Black;
+                Label5.ForeColor = System.Drawing.Color.Black;
+                Label6.ForeColor = System.Drawing.Color.Black;
+                Label7.ForeColor = System.Drawing.Color.Black;
+                Label8.ForeColor = System.Drawing.Color.Black;
+                Label9.ForeColor = System.Drawing.Color.Black;
+                Label10.ForeColor = System.Drawing.Color.Black;
+                Label11.ForeColor = System.Drawing.Color.Black;
+                Label12.ForeColor = System.Drawing.Color.Black;
+                Label13.ForeColor = System.Drawing.Color.Black;
+                Label14.ForeColor = System.Drawing.Color.Black;
+                Label15.ForeColor = System.Drawing.Color.Black;
+                Label16.ForeColor = System.Drawing.Color.Black;
+                Label17.ForeColor = System.Drawing.Color.Black;
+            }
+            else if (DropDownList2.SelectedIndex.Equals(2))
+            {
+                Label1.ForeColor = System.Drawing.Color.Green;
+                Label2.ForeColor = System.Drawing.Color.Green;
+                Label3.ForeColor = System.Drawing.Color.Green;
+                Label4.ForeColor = System.Drawing.Color.Green;
+                Label5.ForeColor = System.Drawing.Color.Green;
+                Label6.ForeColor = System.Drawing.Color.Green;
+                Label7.ForeColor = System.Drawing.Color.Green;
+                Label8.ForeColor = System.Drawing.Color.Green;
+                Label9.ForeColor = System.Drawing.Color.Green;
+                Label10.ForeColor = System.Drawing.Color.Green;
+                Label11.ForeColor = System.Drawing.Color.Green;
+                Label12.ForeColor = System.Drawing.Color.Green;
+                Label13.ForeColor = System.Drawing.Color.Green;
+                Label14.ForeColor = System.Drawing.Color.Green;
+                Label15.ForeColor = System.Drawing.Color.Green;
+                Label16.ForeColor = System.Drawing.Color.Green;
+                Label17.ForeColor = System.Drawing.Color.Green;
+            }
+            else if (DropDownList2.SelectedIndex.Equals(3))
+            {
+                Label1.ForeColor = System.Drawing.Color.Blue;
+                Label2.ForeColor = System.Drawing.Color.Blue;
+                Label3.ForeColor = System.Drawing.Color.Blue;
+                Label4.ForeColor = System.Drawing.Color.Blue;
+                Label5.ForeColor = System.Drawing.Color.Blue;
+                Label6.ForeColor = System.Drawing.Color.Blue;
+                Label7.ForeColor = System.Drawing.Color.Blue;
+                Label8.ForeColor = System.Drawing.Color.Blue;
+                Label9.ForeColor = System.Drawing.Color.Blue;
+                Label10.ForeColor = System.Drawing.Color.Blue;
+                Label11.ForeColor = System.Drawing.Color.Blue;
+                Label12.ForeColor = System.Drawing.Color.Blue;
+                Label13.ForeColor = System.Drawing.Color.Blue;
+                Label14.ForeColor = System.Drawing.Color.Blue;
+                Label15.ForeColor = System.Drawing.Color.Blue;
+                Label16.ForeColor = System.Drawing.Color.Blue;
+                Label17.ForeColor = System.Drawing.Color.Blue;
+            }
+            else if (DropDownList2.SelectedIndex.Equals(4))
+            {
+                Label1.ForeColor = System.Drawing.Color.Red;
+                Label2.ForeColor = System.Drawing.Color.Red;
+                Label3.ForeColor = System.Drawing.Color.Red;
+                Label4.ForeColor = System.Drawing.Color.Red;
+                Label5.ForeColor = System.Drawing.Color.Red;
+                Label6.ForeColor = System.Drawing.Color.Red;
+                Label7.ForeColor = System.Drawing.Color.Red;
+                Label8.ForeColor = System.Drawing.Color.Red;
+                Label9.ForeColor = System.Drawing.Color.Red;
+                Label10.ForeColor = System.Drawing.Color.Red;
+                Label11.ForeColor = System.Drawing.Color.Red;
+                Label12.ForeColor = System.Drawing.Color.Red;
+                Label13.ForeColor = System.Drawing.Color.Red;
+                Label14.ForeColor = System.Drawing.Color.Red;
+                Label15.ForeColor = System.Drawing.Color.Red;
+                Label16.ForeColor = System.Drawing.Color.Red;
+                Label17.ForeColor = System.Drawing.Color.Red;
+            }
+            else if (DropDownList2.SelectedIndex.Equals(5))
+            {
+                Label1.ForeColor = System.Drawing.Color.Purple;
+                Label2.ForeColor = System.Drawing.Color.Purple;
+                Label3.ForeColor = System.Drawing.Color.Purple;
+                Label4.ForeColor = System.Drawing.Color.Purple;
+                Label5.ForeColor = System.Drawing.Color.Purple;
+                Label6.ForeColor = System.Drawing.Color.Purple;
+                Label7.ForeColor = System.Drawing.Color.Purple;
+                Label8.ForeColor = System.Drawing.Color.Purple;
+                Label9.ForeColor = System.Drawing.Color.Purple;
+                Label10.ForeColor = System.Drawing.Color.Purple;
+                Label11.ForeColor = System.Drawing.Color.Purple;
+                Label12.ForeColor = System.Drawing.Color.Purple;
+                Label13.ForeColor = System.Drawing.Color.Purple;
+                Label14.ForeColor = System.Drawing.Color.Purple;
+                Label15.ForeColor = System.Drawing.Color.Purple;
+                Label16.ForeColor = System.Drawing.Color.Purple;
+                Label17.ForeColor = System.Drawing.Color.Purple;
+            }
+            else if (DropDownList2.SelectedIndex.Equals(6))
+            {
+                Label1.ForeColor = System.Drawing.Color.Orange;
+                Label2.ForeColor = System.Drawing.Color.Orange;
+                Label3.ForeColor = System.Drawing.Color.Orange;
+                Label4.ForeColor = System.Drawing.Color.Orange;
+                Label5.ForeColor = System.Drawing.Color.Orange;
+                Label6.ForeColor = System.Drawing.Color.Orange;
+                Label7.ForeColor = System.Drawing.Color.Orange;
+                Label8.ForeColor = System.Drawing.Color.Orange;
+                Label9.ForeColor = System.Drawing.Color.Orange;
+                Label10.ForeColor = System.Drawing.Color.Orange;
+                Label11.ForeColor = System.Drawing.Color.Orange;
+                Label12.ForeColor = System.Drawing.Color.Orange;
+                Label13.ForeColor = System.Drawing.Color.Orange;
+                Label14.ForeColor = System.Drawing.Color.Orange;
+                Label15.ForeColor = System.Drawing.Color.Orange;
+                Label16.ForeColor = System.Drawing.Color.Orange;
+                Label17.ForeColor = System.Drawing.Color.Orange;
+            }
+            else if (DropDownList2.SelectedIndex.Equals(7))
+            {
+                Label1.ForeColor = System.Drawing.Color.Yellow;
+                Label2.ForeColor = System.Drawing.Color.Yellow;
+                Label3.ForeColor = System.Drawing.Color.Yellow;
+                Label4.ForeColor = System.Drawing.Color.Yellow;
+                Label5.ForeColor = System.Drawing.Color.Yellow;
+                Label6.ForeColor = System.Drawing.Color.Yellow;
+                Label7.ForeColor = System.Drawing.Color.Yellow;
+                Label8.ForeColor = System.Drawing.Color.Yellow;
+                Label9.ForeColor = System.Drawing.Color.Yellow;
+                Label10.ForeColor = System.Drawing.Color.Yellow;
+                Label11.ForeColor = System.Drawing.Color.Yellow;
+                Label12.ForeColor = System.Drawing.Color.Yellow;
+                Label13.ForeColor = System.Drawing.Color.Yellow;
+                Label14.ForeColor = System.Drawing.Color.Yellow;
+                Label15.ForeColor = System.Drawing.Color.Yellow;
+                Label16.ForeColor = System.Drawing.Color.Yellow;
+                Label17.ForeColor = System.Drawing.Color.Yellow;
+            }
+        }
     }
 }
--- Learningweb/TheLionandTheMouseShortStory.aspx
+++ Learningweb/TheLionandTheMouseShortStory.aspx
@@ -15,56 +15,80 @@
 <body>
     <form id="form1" runat="server">
         <div style="text-align: center">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <asp:DropDownList ID="DropDownList2" runat="server" AutoPostBack="True" Height="33px" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged" style="text-align: center; font-weight: 700">
+                <asp:ListItem>Select color</asp:ListItem>
+                <asp:ListItem>Black</asp:ListItem>
+                <asp:ListItem>Green</asp:ListItem>
+                <asp:ListItem>Blue</asp:ListItem>
+                <asp:ListItem>Red</asp:ListItem>
+                <asp:ListItem>Purple</asp:ListItem>
+                <asp:ListItem>Orange</asp:ListItem>
+                <asp:ListItem>Yellow</asp:ListItem>
+            </asp:DropDownList>
+        </div>
+        <div style="text-align: center">
             <h2><strong>The Lion and The Mouse </strong></h2>
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/1.jpg" /><br />
-            <strong><em>lets start reading<br />
+            <asp:Label ID="Label1" runat="server" Text="lets start reading"></asp:Label>
+            <strong><em><br />
             </em></strong>
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/2.jpg" /><br />
-            There was a beautiful forest.<br />
+            <asp:Label ID="Label2" runat="server" Text="There was a beautiful forest."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/3.jpg" /><br />
-            There lived a mouse near a lion&#39;s cage.<br />
+            <asp:Label ID="Label3" runat="server" Text="There lived a mouse near a lion's cage."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/4.jpg" /><br />
-            The mouse was very playful.<br />
+            <asp:Label ID="Label4" runat="server" Text="The mouse was very playful."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/5.jpg" /><br />
-            One day the mouse went to lion&#39;s cage.<br />
+            <asp:Label ID="Label5" runat="server" Text="One day the mouse went to lion's cage."></asp:Label>
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/6.jpg" /><br />
-            Without knowing the lion&#39;s power it started to play over the lion.<br />
+            <asp:Label ID="Label6" runat="server" Text="Without knowing the lion's power it started to play over the lion."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/7.jpg" /><br />
-            Lion suddenly woke up. It got angry and began to roar loudly.<br />
+            <asp:Label ID="Label7" runat="server" Text="Lion suddenly woke up. It got angry and began to roar loudly."></asp:Label>
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/8.jpg" /><br />
-            Lion caught the mouse and threatened that he would eat him.<br />
+            <asp:Label ID="Label8" runat="server" Text="Lion caught the mouse and threatened that he would eat him."></asp:Label>
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/9.jpg" /><br />
-            Mouse said sorry and pleaded the lion to leave him without eating.<br />
+            <asp:Label ID="Label9" runat="server" Text="Mouse said sorry and pleaded the lion to leave him without eating."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/10.jpg" /><br />
-            The lion took pity and let the mouse to go.<br />
+            <asp:Label ID="Label10" runat="server" Text="The lion took pity and let the mouse to go."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/11.jpg" /><br />
-            Two days later, a hunter came to the forest.<br />
+            <asp:Label ID="Label11" runat="server" Text="Two days later, a hunter came to the forest."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/12.jpg" /><br />
-            The hunter saw the lion and trapped it using his net.<br />
+            <asp:Label ID="Label12" runat="server" Text="The hunter saw the lion and trapped it using his net."></asp:Label>
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/13.jpg" /><br />
-            The hunter went to his village to find more people to kill the lion. Lion kept shouting for help.<br />
+            <asp:Label ID="Label13" runat="server" Text="The hunter went to his village to find more people to kill the lion. Lion kept shouting for help."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/14.jpg" /><br />
-            Mouse heard lion&#39;s cries and came to help him. It cut the net in to pieces and freed the lion.<br />
+            <asp:Label ID="Label14" runat="server" Text="Mouse heard lion's cries and came to help him. It cut the net in to pieces and freed the lion."></asp:Label>
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/15.jpg" /><br />
-            Finally, the lion escaped from the net.<br />
+            <asp:Label ID="Label15" runat="server" Text="Finally, the lion escaped from the net."></asp:Label>
+            <br />
             <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/16.jpg" /><br />
             <br />
-            The lion thanked the mouse for his help. Then, they two started to live as friends forever.<br />
+            <asp:Label ID="Label16" runat="server" Text="The lion thanked the mouse for his help. Then, they two started to live as friends forever."></asp:Label>
+            <br />
             <img alt="" class="auto-style1" src="images%20(2)/The%20Lion%20and%20The%20Mouse%20Short%20Story/17.jpg" /><br />
             <br />
             <br />
